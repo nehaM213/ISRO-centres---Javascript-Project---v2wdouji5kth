9847575496012d3c9e7ef79648ea738eff479164
Neha Maurya <nehamaurya213@gmail.com>
2023-06-14 17:43:08 +0530

    Responsive & error handling

--- App.js
+++ App.js
@@ -1,29 +1,30 @@
 const api_url = "https://isro.vercel.app/api/centres";
 
-showData();
-
+loadData();
+let sortData = {};
 async function getapi(url) {
-  const response = await fetch(url);
-  let data = await response.json();
-  if (response) {
+  try {
+    const response = await fetch(url);
+    let data = await response.json();
     hideloader();
+    return data;
+  } catch (err) {
+    document.getElementById("error").textContent =
+      "check your internet connection!";
   }
-  return data;
 }
 
-function hideloader() {
-  document.getElementById("loading").style.display = "none";
-}
-
-async function showData() {
+async function loadData() {
   let data = await getapi(api_url);
-  dispaly(data.centres);
+  sortData = data.centres;
+  dispalyData(data.centres);
 }
 
-function dispaly(data) {
+function dispalyData(data) {
   let row = ``;
   for (let centre of data) {
     row += `<tr> 
+        <td>${centre.id}</td>
         <td><span>CENTER</span>${centre.name} </td>
         <td><span>CITY</span>${centre.Place}</td>
         <td><span>STATE</span>${centre.State}</td>         
@@ -32,42 +33,40 @@ function dispaly(data) {
   document.getElementById("list").innerHTML = row;
 }
 
-async function search(filter) {
+async function search() {
   let data = await getapi(api_url);
   let searchValue = document.getElementById("search").value;
   let list = [];
-  if (filter == "city" && searchValue!="") {
-    for (let centre of data.centres) {
-      if (centre.Place.toUpperCase() == searchValue.toUpperCase()) {
-        list.push(centre);
-      }
-    }
-  } else if (filter == "state" && searchValue != "") {
-    for (let centre of data.centres) {
-      if (centre.State.toUpperCase() == searchValue.toUpperCase()) {
-        list.push(centre);
-      }
-    }
-  } else if (filter == "centre" && searchValue != "") {
-    for (let centre of data.centres) {
-      if (centre.name.toUpperCase() == searchValue.toUpperCase()) {
-        list.push(centre);
-      }
-    }
-  } else {
-    if (searchValue == "") {
-      dispaly(data.centres);
-    }
-    searchValue = searchValue.toUpperCase();
-    for (let centre of data.centres) {
-      if (
-        centre.name.toUpperCase().indexOf(searchValue) > -1 ||
-        centre.Place.toUpperCase().indexOf(searchValue) > -1 ||
-        centre.State.toUpperCase().indexOf(searchValue) > -1
-      ) {
-        list.push(centre);
-      }
+  if (searchValue == "") {
+    sortData = data.centres;
+    dispalyData(data.centres);
+  }
+  searchValue = searchValue.toUpperCase();
+  for (let centre of data.centres) {
+    if (
+      centre.name.toUpperCase().indexOf(searchValue) > -1 ||
+      centre.Place.toUpperCase().indexOf(searchValue) > -1 ||
+      centre.State.toUpperCase().indexOf(searchValue) > -1
+    ) {
+      list.push(centre);
     }
   }
-  dispaly(list);
+  sortData = list;
+  dispalyData(list);
+}
+async function sort(filter) {
+  console.log(sortData);
+  let sortedDta = {};
+  if (filter == "city") {
+    sortedDta = sortData.sort((a, b) => (a.Place > b.Place ? 1 : -1));
+  } else if (filter == "state") {
+    sortedDta = sortData.sort((a, b) => (a.State > b.State ? 1 : -1));
+  } else if (filter == "centre") {
+    sortedDta = sortData.sort((a, b) => (a.name > b.name ? 1 : -1));
+  }
+  dispalyData(sortedDta);
+}
+
+function hideloader() {
+  document.getElementById("loading").style.display = "none";
 }
